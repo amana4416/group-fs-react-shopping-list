104eaf29240eee4cc109fb593ef5e4daf7d26d72
Tou Fue Yang <touyangbiz@gmail.com>
2023-01-12 14:49:18 -0600

    created table sql

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "ShoppingList"(
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(200) NOT NULL,
+    "quanity" DECIMAL (2,2),
+    "unit" VARCHAR(50), 
+    "is_purchased" BOOLEAN DEFAULT FALSE    
+)
+
+INSERT INTO "ShoppingList" ("name", "quantity", "unit")
+VALUES
+('Bread', '2', 'loaves');
