1b5efd14f83ea112461a8d059341a22ff26ca066
Aman Abdirazak <amanabdirizak@icloud.com>
2023-01-13 12:41:19 -0600

    got both delete routes working

--- src/components/shoppingList/shoppingList.js
+++ src/components/shoppingList/shoppingList.js
@@ -1,11 +1,44 @@
-function ShoppingList({shoppingList}) {
+import axios from 'axios';
+
+function ShoppingList({shoppingList, getShoppingList}) {
+    
+    //delete request deleting a single item
+    const removeItem = (id) => {
+        axios.delete(`/items/${id}`)
+        .then(response => {
+            console.log('deleting an item from the shopping list');
+            getShoppingList();
+        })
+        .catch(error => {
+            console.log('error in client delete route - just deleting one item', error);
+            alert('error deleting just one item');
+        })
+    }
+    
+    //delete request clearing the whole list
+    const clearList = () => {
+        axios.delete(`/items`)
+        .then(response => {
+            console.log('clearing the whole shopping list');
+            getShoppingList();
+        })
+        .catch(error => {
+            console.log('error in client delete route - clearing the whole list', error);
+            alert('error error clearing the list');
+        })
+    }
+
     return (
         <>
+            <button onClick={clearList}>Clear</button>
             <table>
                 <thead>
-                    <th>Item Name</th>
-                    <th>Quantity</th>
-                    <th>Unit</th>
+                    <tr>
+                        <th>Item Name</th>
+                        <th>Quantity</th>
+                        <th>Unit</th>
+                        <th>Remove</th>
+                    </tr>
                 </thead>
                 <tbody>
                     {shoppingList.map(item => (
@@ -13,6 +46,9 @@ function ShoppingList({shoppingList}) {
                             <td>{item.name}</td>
                             <td>{item.quantity}</td>
                             <td>{item.unit}</td>
+                            <td>
+                                <button onClick={()=>removeItem(item.id)}>Remove</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
